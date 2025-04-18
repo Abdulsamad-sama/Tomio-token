e00a1106911d1f429fb7697d25b0aa39184f3546
Abdulsamad Hamzat <hamzatabdulsamad34@gmail.com>
2025-04-18 11:20:39 +0100

    no more zoros

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,7 +4,7 @@
 function Footer() {
   const date = new Date();
   return (
-    <footer className="flex flex-col items-center justify-center gap-4 p-4 bg-gray-200 text-gray-700">
+    <footer className="flex flex-col items-center justify-center gap-4 p-4 border-t-2 border-amber-600 text-gray-700">
       <h2>Tomio.</h2>
       <p>Copyright @{date.getFullYear()} Tomio. All rights reserved.</p>
     </footer>
